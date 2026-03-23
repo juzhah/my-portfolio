224954fefb495226ca64aeef1f7b08fd37166d7c
Juan Esteban Lozada Hidalgo <juanlozadahe@gmail.com>
2026-03-23 02:50:11 -0500

    feat(ui-media-lock): add video element to render video assets

--- frontend/components/blog-components/Media.tsx
+++ frontend/components/blog-components/Media.tsx
@@ -1,5 +1,6 @@
 import { StrapiImage } from "@/app/types";
 import { mapStrapiImage } from "@/lib/map-strapi-image";
+import { div } from "motion/react-client";
 import Image from "next/image";
 
 export default function Media({ media }: { media: StrapiImage }) {
@@ -8,17 +9,53 @@ export default function Media({ media }: { media: StrapiImage }) {
 
   const { src, width, height, alt } = image;
 
+  const mediaType: string = media.provider_metadata.resource_type;
+
   return (
-    <div className="max-w-full rounded-md overflow-clip border">
-      <Image
-        src={src}
-        alt={alt}
-        width={width}
-        height={height}
-        loading="lazy"
-        quality={75}
-        className="w-full h-auto"
-      />
-    </div>
+    <>
+      {mediaType === "image" && (
+        <figure>
+          <div className="max-w-full rounded-md overflow-clip border">
+            <Image
+              src={src}
+              alt={alt}
+              width={width}
+              height={height}
+              loading="lazy"
+              quality={75}
+              className="w-full h-auto"
+            />
+          </div>
+          {media.caption && (
+            <figcaption className="pl-4 pt-4 text-gray-500 flex gap-4 italic">
+              <div className="bg-primary/60 w-1 h-lh"></div>
+              {media.caption}
+            </figcaption>
+          )}
+        </figure>
+      )}
+
+      {mediaType === "video" && (
+        <figure>
+          <div className="max-w-full rounded-md overflow-clip border">
+            <video
+              src={src}
+              autoPlay
+              muted
+              playsInline
+              loop
+              preload="none"
+              className="w-full h-auto"
+            />
+          </div>
+          {media.caption && (
+            <figcaption className="pl-4 pt-4 text-gray-500 flex gap-4 italic">
+              <div className="bg-primary/60 w-1 h-lh"></div>
+              {media.caption}
+            </figcaption>
+          )}
+        </figure>
+      )}
+    </>
   );
 }
